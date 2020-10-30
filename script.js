f253b98faa0dd541939ca1d7d350a615754a4f4f
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-10-30 14:39:39 +0100

    Sorting done

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ const bands = [
   'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive',
   'Anywhere But Here', 'An Old Dog'
 ];
+
+// This would be the 'basic' sorting - which includes articles
+/*
+const sortedBands = bands.sort(function (a, b) {
+  if (a > b) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+console.log(sortedBands);
+*/
+
+// RegExp replacing articles with 'nothing'
+// Then we 'trim' any spaces that might remain  in the end
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+/*
+const sortedBands = bands.sort(function (a, b) {
+  if (a > b) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+*/
+
+// Simpler version
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+// Takes the element, sets its innerHTML to the bandname
+// Displays them in a list
+// 'join' is needed because the string version (toString) of sortedBands array would contain
+// a list of names divided by commas
+document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`)
+  .join('');
+
+console.log(sortedBands);
